7726bbf4c7f47562f6a47c5a4729bdfb6c68e5bc
Julien Diot <juliendiot42@gmail.com>
2018-07-08 11:11:29 +0200

    refactor(admin): remove ui code from "server", source ui_admin_loggedIn

---
+++ src/ui_admin_loggedIn.R
@@ -0,0 +1,204 @@
+## Copyright 2015,2016,2017,2018 Institut National de la Recherche Agronomique
+## and Montpellier SupAgro.
+##
+## This file is part of PlantSelBreedGame.
+##
+## PlantSelBreedGame is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Affero General Public License as
+## published by the Free Software Foundation, either version 3 of the
+## License, or (at your option) any later version.
+##
+## PlantSelBreedGame is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU Affero General Public
+## License along with PlantSelBreedGame.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+
+##### File information #####
+## This file contain the UI code for logged user for the admin tab
+## this file is sourced in "server_admin.R" in a renderUI() function
+############################
+
+list(
+    shinydashboard::tabBox(width=12, title = "Admin", id = "admin_tabset", side="left",
+                           tabPanel("Manage sessions",
+                                    div(style="margin-bottom:50px;",
+                                        h3("Current sessions:"),
+                                        tableOutput("sessionsTable")
+                                    ),
+                                    
+                                    div(# add New session
+                                        div(style="margin-bottom: 20px;",# inputs
+                                            h3("Add a new session:"),
+                                            div(style="display: inline-block; vertical-align:top; width: 33%; min-width:300px;", #start
+                                                h4("Start"),
+                                                tags$table(style = "width: 300px; border-collapse: collapse;", # start table 1
+                                                           tags$td(style = "width: 34%; vertical-align: bottom; padding: 10px; padding-bottom: 13.8px;",
+                                                                   dateInput("startDate", "date",
+                                                                             width = "100px")
+                                                           ),
+                                                           tags$td(style = "width: 33%; vertical-align: bottom; padding: 10px; padding-bottom: 13.8px;",
+                                                                   numericInput("startHour", "hour", value=9, min=0, max=23, step=1,
+                                                                                width = "75px")
+                                                           ),
+                                                           tags$td(style = "width: 33%; vertical-align: bottom; padding: 10px; padding-bottom: 13.8px;",
+                                                                   numericInput("startMin", "minute", value=0, min=0, max=59, step=1,
+                                                                                width = "75px")
+                                                           )
+                                                )# end table 1
+                                            ),# end div "start"
+                                            
+                                            div(style="display: inline-block; vertical-align:top; width: 33%; min-width:300px;", #end
+                                                h4("End"),
+                                                tags$table(style = "width: 300px; border-collapse: collapse;", # start table 2
+                                                           tags$td(style = "width: 34%; vertical-align: bottom; padding: 10px; padding-bottom: 13.8px;",
+                                                                   dateInput("endDate", "date",
+                                                                             width = "100px")
+                                                           ),
+                                                           tags$td(style = "width: 33%; vertical-align: bottom; padding: 10px; padding-bottom: 13.8px;",
+                                                                   numericInput("endHour", "hour", value=9, min=0, max=23, step=1,
+                                                                                width = "75px")
+                                                           ),
+                                                           tags$td(style = "width: 33%; vertical-align: bottom; padding: 10px; padding-bottom: 13.8px;",
+                                                                   numericInput("endMin", "minute", value=0, min=0, max=59, step=1,
+                                                                                width = "75px")
+                                                           )
+                                                )# end table 2
+                                            ),# end div "end"
+                                            
+                                            div(style="display: inline-block; vertical-align:top; width:33%; min-width:300px;", #year time
+                                                h4("Year time"),
+                                                tags$table(style = "border-collapse: collapse;", # start table 3
+                                                           tags$td(style = "width: 33%; vertical-align: bottom; padding: 10px; padding-bottom: 13.8px;",
+                                                                   numericInput("yearTime", "Duration of one year (in minutes)", value=60, min=0, max=Inf, step=1)
+                                                           )
+                                                )# end table 3
+                                                
+                                            )# end div "year time"
+                                        ),# end div inputs
+                                        
+                                        div(style="display: inline-block; vertical-align:top; width:25%; margin-bottom: 50px; padding-left: 10px;", #button
+                                            actionButton("addSession", "Add this new session")
+                                            
+                                        )# end div "button"
+                                        
+                                        
+                                    ),# end div "add New session"
+                                    
+                                    
+                                    
+                                    div(style = "margin-bottom:100px;",# delete session
+                                        h3("Delete sessions:"),
+                                        tags$table(style = "width: 100%; border-collapse: collapse;",
+                                                   tags$td(style = "width: 50%; vertical-align: bottom; padding: 10px;",
+                                                           selectInput("delSession", "Session's number",choices=c("",sessionsList()$num),
+                                                                       selected="", width="100%")
+                                                   ),
+                                                   tags$td(style = "width: 50%; vertical-align: bottom; padding: 10px; padding-bottom: 13.8px;",
+                                                           actionButton("deleteSession", "DO NOT click! (unless you are sure to delete this session)",
+                                                                        width="100%", style="margin-bottom: 0px;",
+                                                                        style="background-color:#ff3333; color:white;")
+                                                   )
+                                        )
+                                    )# end div "delete session"
+                           ),# end tabPanel sessions managment
+                           
+                           
+                           
+                           
+                           
+                           
+                           tabPanel("Manage breeders",
+                                    div(# add New breeders
+                                        h3("Add a new breeder:"),
+                                        tags$head(
+                                            tags$style(HTML('.shiny-input-container{margin-bottom: 0px;}
+                                                            .selectize-control{margin-bottom: 0px;}')
+                                            )
+                                            ),
+                                        tags$table(style = "width: 100%; border-collapse: collapse;",
+                                                   tags$tr(
+                                                       tags$td(style = "width: 25%; vertical-align: bottom; padding: 10px; padding-bottom: 13.8px;",
+                                                               textInput("newBreederName", "Breeder's name", placeholder="spaces forbidden",
+                                                                         width="100%")
+                                                       ),
+                                                       tags$td(style = "width: 25%; vertical-align: bottom; padding: 10px;",
+                                                               selectInput("newBreederStatus", "Status", choices=c("player","tester","game master"),
+                                                                           width="100%")
+                                                       ),
+                                                       tags$td(style = "width: 25%; vertical-align: bottom; padding: 10px; padding-bottom: 13.8px;",
+                                                               passwordInput("newBreederPsw", "Password",
+                                                                             width="100%")
+                                                       ),
+                                                       tags$td(style = "width: 25%; vertical-align: bottom; padding: 10px; padding-bottom: 13.8px;",
+                                                               actionButton("addNewBreeder", "Add this new breeder",
+                                                                            width="100%", style="margin-bottom: 0px;")
+                                                       )
+                                                   )
+                                        )# end tags$table
+                                        ),# end div "add new breeder"
+                                    
+                                    
+                                    div(# delete breeders
+                                        h3("Delete a breeder:"),
+                                        tags$table(style = "width: 100%; border-collapse: collapse;",
+                                                   tags$td(style = "width: 50%; vertical-align: bottom; padding: 10px;",
+                                                           selectInput("delBreederName", "Breeder's name",choices=c("",breederList()),
+                                                                       selected="", width="100%")
+                                                   ),
+                                                   tags$td(style = "width: 50%; vertical-align: bottom; padding: 10px; padding-bottom: 13.8px;",
+                                                           actionButton("deleteBreeder", "DO NOT click! (unless you are sure to delete this breeder)",
+                                                                        width="100%", style="margin-bottom: 0px;",
+                                                                        style="background-color:#ff3333; color:white;")
+                                                   )
+                                        )
+                                    )# end div "delete breeders"
+    ),# end tabPanel "Add/Delete Breeders"
+    
+    
+    
+    
+    
+    tabPanel("Disk usage",
+             div(id="admin_diskU_data",
+                 style="display: inline-block;
+                 vertical-align:top;
+                 width: 33%;
+                 min-width:300px;",
+                 h3("Disk usage:"),
+                 tableOutput("sizeDataFolder")
+             ),
+             div(id="admin_diskU_input",
+                 style="display: inline-block;
+                 vertical-align:top;
+                 width: 66%;",
+                 p("To prevent over disk usage on your server, you can specifiy here a maximum size for all game data allowed.",
+                   style="margin-top:20px;"),
+                 p("In case the size of all data exeed this maximum, players will not be allowed to connect any more.
+                   You will have to delete the haplotype of some breeders individuals."),
+                 p(textOutput("InfoCurrentMaxDiskUsage")),
+                 div(style="width: 50%;
+                     display: inline-block;
+                     vertical-align: top;",
+                     numericInput("admin_maxDiskUsage",
+                                  label= "Maximum disk usage (in Mo)",
+                                  value= 10000,
+                                  min = 2000)
+                 ),
+                 div(style="width: 30%;
+                     padding-top: 26px;
+                     display: inline-block;
+                     vertical-align: top;",
+                     actionButton("updateMaxDiskUsage",
+                                  label="Update")
+                 )
+                 
+                 
+    )
+    )# end tabPanel "Disk usage"
+    )# close tabBox
+             ) # close list
