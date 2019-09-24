8f4d83e44c3748052d1ab8cd6d71ffa12532a196
Julien Diot <juliendiot@ut-biomet.org>
2019-09-24 16:13:43 +0900

    doc(plt-mat, pheno, geno): add information on each module pages.

--- src/ui_plant_material.R
+++ src/ui_plant_material.R
@@ -30,6 +30,60 @@ tabItem(tabName="plant_mat",
 
   uiOutput("UIbreederInfoPltMat"),
 
+  shinydashboard::box(width = 12, title = "Request plant material",
+    div(id = "cross_info1",
+        p("In this module, you can request new plant materials."),
+        p("A greenhouse can be used", strong("all year long"), " to perform crosses:"),
+        tags$ul(
+          tags$li(strong("Allofecundation"),": ", constants$duration.allof,"-month delay and costs", constants$cost.allof, " plot (", format(constants$cost.allof * constants$cost.pheno.field, digits = 2), " Mendels )."),
+          tags$li(strong("Autofecundation"),": ", constants$duration.autof,"-month delay and costs", constants$cost.autof, " plot (", format(constants$cost.autof * constants$cost.pheno.field, digits = 2), " Mendels ).")
+        ),
+        p("A laboratory can also be used to perform ", strong("haplodiploidisation"), ". It has a ", constants$duration.haplodiplo, "-month delay, costs ", constants$cost.haplodiplo, " plot (", format(constants$cost.haplodiplo * constants$cost.pheno.field, digits=2), " Mendels ), and a maximum of ", constants$max.nb.haplodiplos, " can be requested at once.")
+    ),
+
+    div(id = "cross_info2",
+        p("The request file for this module should be similar to the following example:"),
+        tags$pre(HTML("<table>
+                        <tr>
+                        <td>parent1\t</td>
+                        <td>parent2\t</td>
+                        <td>child\t</td>
+                        <td>explanations</td>
+                        </tr>
+                        <tr>
+                        <td>Coll0001\t</td>
+                        <td>Coll0002\t</td>
+                        <td>0001-0002.1\t</td>
+                        <td>allofecundation</td>
+                        </tr>
+                        <tr>
+                        <td>Coll0001\t</td>
+                        <td>Coll0001\t</td>
+                        <td>0001-0001.1\t</td>
+                        <td>autofecundation</td>
+                        </tr>
+                        <tr>
+                        <td>Coll0001\t</td>
+                        <td>\t</td>
+                        <td>0001-HD.1\t</td>
+                        <td>haplodiploidization</td>
+                        </tr>
+                        </table>")
+                ),
+
+        p(tags$ul(
+          tags$li("The file should be in", code(".txt"), "format with", strong("tabulations"), "separator and ", strong(code("UTF-8"), "encoding.")),
+          tags$li("Each row corresponds to a child."),
+          tags$li("Only columns", code("parent1"),", ",code("parent2"),", and ", code("child"), " are compulsory."),
+          tags$li("Only the ",code("parent2")," column can be empty (which means haplodiploidization request)."),
+          tags$li("Individual names should only use ",code("[a-z], [A-Z], [0-9], [._-]")," (no space, comma, etc)."),
+          tags$li("Individual names should be unique."),
+          tags$li("Lines starting with ",code("#")," will be ignored.")
+
+        ))
+    )
+
+  ),
 
   shinydashboard::box(width=12, title = "Choose an instruction file for plant material:",
                       div( id="cross_file",
