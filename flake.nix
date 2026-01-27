a9cb902e5b08fe3e7cd34479bca4ec1da091e709
Julien DIOT <jd42-git@protonmail.com>
2026-01-27 10:40:50 +0900

    temp fix(docker): nix2container's can build skopeo

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     flake-utils.url = "github:numtide/flake-utils";
 
     nix2container.url = "github:nlewo/nix2container";
-    nix2container.inputs.nixpkgs.follows = "nixpkgs";
+    # nix2container.inputs.nixpkgs.follows = "nixpkgs"; # TODO: problem building skopeo
   };
 
   outputs =
